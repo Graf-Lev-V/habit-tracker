b23692fc9cde5e6b26dec2838d22e5245776b018
Graf-Lev-V <legendarlev2004@gmail.com>
2026-06-20 00:06:29 +0300

    update

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -1,8 +1,9 @@
 export function calendar() {
     const dates = []
-    
-    for (let i = 0; i <= 365; i++) {
-        dates.push(new Date(2026, 1, 1).toISOString().split('T')[0])
+    const date = new Date()
+    for (let d = 0; d <= 365; d++) {
+        dates.push(date.toISOString().split('T')[0])
+        date.setDate(date.getDate() - 1);
     }
 
     return

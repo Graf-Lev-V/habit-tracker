06905318be6959065c952c4b0612dbf02261da63
Graf-Lev-V <legendarlev2004@gmail.com>
2026-06-20 00:24:14 +0300

    update

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -1,10 +1,10 @@
-export function calendar() {
+export function calendar(completedDates: Array<string>) {
     const dates = []
     const date = new Date()
     for (let d = 0; d <= 365; d++) {
         dates.push(date.toISOString().split('T')[0])
         date.setDate(date.getDate() - 1);
     }
-
-    return
+    const result = dates.map((date) => completedDates.includes(date) ? {date: date, completed: true} : {date: date, completed: false})
+    return result
 }

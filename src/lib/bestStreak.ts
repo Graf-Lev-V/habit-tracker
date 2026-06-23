3065e12b23b5b742d64afb8344e1a40bc0350960
Graf-Lev-V <legendarlev2004@gmail.com>
2026-06-23 05:42:49 +0300

    update

--- src/lib/bestStreak.ts
+++ src/lib/bestStreak.ts
@@ -2,21 +2,23 @@ export function calculateBestStreak(completedDates: Array<string>) {
     if (completedDates.length === 0) return 0;
     const uniqueDates = [...new Set(completedDates)]
     uniqueDates.sort((a, b) => b.localeCompare(a) )
-    let streak = 1;
-    for (let i = 1; i < uniqueDates.length; i++) {
-        if (new Date(uniqueDates[i-1]).valueOf() - new Date(uniqueDates[i]).valueOf() > 86400000) break
-        streak += 1;
+    let maxStreak = 1;
+    for (let day = 1; day < uniqueDates.length; day++) {
+        const { streak, nextDay } = calculateStreak({day, uniqueDates})
+        day = nextDay;
+        maxStreak = maxStreak > streak ? maxStreak : streak;
     }
-    return streak;
+    return maxStreak
 }
 
-function calculateStreak(day: <number>) {
-    const streak = 1;
-    while (true) {
-        if (new)
+function calculateStreak({ day, uniqueDates }: {day: number; uniqueDates: Array<string>}) {
+    let streak = 1;
+    let nextDay = day;
+    while (day < uniqueDates.length) {
+        if (new Date(uniqueDates[day-1]).valueOf() - new Date(uniqueDates[day]).valueOf() > 86400000) break
+        streak += 1;
+        nextDay += 1;
+        day += 1;
     }
-    
-
-
-
+    return { streak, nextDay }
 }

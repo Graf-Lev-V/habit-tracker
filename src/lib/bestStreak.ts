5e9ddb0a637cd0816b76d6db62082fbda639d5a8
Graf-Lev-V <legendarlev2004@gmail.com>
2026-06-23 04:21:24 +0300

    update

---
+++ src/lib/bestStreak.ts
@@ -0,0 +1,22 @@
+export function calculateBestStreak(completedDates: Array<string>) {
+    if (completedDates.length === 0) return 0;
+    const uniqueDates = [...new Set(completedDates)]
+    uniqueDates.sort((a, b) => b.localeCompare(a) )
+    let streak = 1;
+    for (let i = 1; i < uniqueDates.length; i++) {
+        if (new Date(uniqueDates[i-1]).valueOf() - new Date(uniqueDates[i]).valueOf() > 86400000) break
+        streak += 1;
+    }
+    return streak;
+}
+
+function calculateStreak(day: <number>) {
+    const streak = 1;
+    while (true) {
+        if (new)
+    }
+    
+
+
+
+}

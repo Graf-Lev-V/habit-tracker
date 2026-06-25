9831f93dc69dd3810ee7c1f468a0ff6d9bf7af14
Graf-Lev-V <legendarlev2004@gmail.com>
2026-06-25 23:04:40 +0300

    update

--- src/lib/thirtyDay.ts
+++ src/lib/thirtyDay.ts
@@ -2,7 +2,7 @@ export default function ThirtyDayCompletion(completedDates: Array<string>) {
     const uniqueDates = [...new Set(completedDates)]
     let thirtyDayCompleted = 0;
     for (let i = 0; i <= 30; i++) {
-        if (uniqueDates[i] === (new Date().getDate() - 1).toString().split('T')[0]) thirtyDayCompleted += 1;
+        if (uniqueDates.includes(new Date(Date.now() - i * 86400000).toISOString().split('T')[0])) thirtyDayCompleted += 1;
     }
     return thirtyDayCompleted / 30 * 100
 }

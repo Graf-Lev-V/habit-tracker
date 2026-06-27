bb58f4717b2fb6a9f10d9f0bb7da684618f16619
Graf-Lev-V <legendarlev2004@gmail.com>
2026-06-27 23:50:19 +0300

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { calendar } from '@/lib/calendar';
 import { calculateBestStreak } from '@/lib/bestStreak';
 import ThirtyDayCompletion from '@/lib/thirtyDay';
 import AddHabitForm from '@/components/AddHabitForm';
+import HabitCard from '@/components/HabitCard';
 
 export const dynamic = 'force-dynamic'
 
@@ -54,30 +55,7 @@ export default async function Dashboard() {
         </div>
       </div>
       {habitStreak?.map((habit) => 
-          <div key={habit.habit.id}>
-            <p className='text-white'>{habit.habit.name}</p>
-            <p>Streak: {habit.streak}</p>
-            <p>Thirty-day complection: {habit.thirtyDay}%</p>
-            <form action={deleteHabit.bind(null, habit.habit.id)}>
-              <button type='submit'>Delete</button>
-            </form>
-            <form action={toggleHabit.bind(null, habit.habit.id)}>
-              <button type='submit'>Done</button>
-            </form>
-            <div style={{
-              display: 'grid', 
-              gridTemplateRows: 'repeat(7, min-content)', 
-              gridTemplateColumns: 'repeat(53, min-content)', 
-              gap: '4px',
-              gridAutoFlow: 'column'
-            }}>
-              {habit.calendar.map((date) => 
-                date.completed ? 
-                <div key={date.date} className='w-3 h-3 bg-green-600 rounded-xs'></div> :
-                <div key={date.date} className='w-3 h-3 bg-gray-700 rounded-xs'></div>
-              )}
-            </div>
-          </div>
+          <HabitCard key={habit.habit.id} habit={habit}/>
       )}
       <AddHabitForm />
     </main>
---
+++ src/components/HabitCard.tsx
@@ -0,0 +1,37 @@
+type HabitCardProps = {
+    habit: {
+        habit: habit, 
+        streak: calculateStreak(completedDates!), 
+        thirtyDay: ThirtyDayCompletion(completedDates!), 
+        calendar: completedCalendar
+    }
+}
+
+export default function HabitCard(habit: object) {
+    return (
+        <div>
+            <p className='text-white'>{habit.habit.name}</p>
+            <p>Streak: {habit.streak}</p>
+            <p>Thirty-day complection: {habit.thirtyDay}%</p>
+            <form action={deleteHabit.bind(null, habit.habit.id)}>
+                <button type='submit'>Delete</button>
+            </form>
+            <form action={toggleHabit.bind(null, habit.habit.id)}>
+                <button type='submit'>Done</button>
+            </form>
+            <div style={{
+                display: 'grid', 
+                gridTemplateRows: 'repeat(7, min-content)', 
+                gridTemplateColumns: 'repeat(53, min-content)', 
+                gap: '4px',
+                gridAutoFlow: 'column'
+            }}>
+            {habit.calendar.map((date) => 
+                date.completed ? 
+                <div key={date.date} className='w-3 h-3 bg-green-600 rounded-xs'></div> :
+                <div key={date.date} className='w-3 h-3 bg-gray-700 rounded-xs'></div>
+            )}
+            </div>
+        </div>
+    )
+}

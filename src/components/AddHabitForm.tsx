bb69434f7d3a62a2eef91c0dde6b274d704a391c
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-01 04:01:18 +0300

    update

--- src/components/AddHabitForm.tsx
+++ src/components/AddHabitForm.tsx
@@ -8,7 +8,7 @@ export default function AddHabitForm() {
     const [formState, setFormState] = useState<boolean>(false)
 
     return (
-        <div className="flex flex-col gap-2 items-center">
+        <div className="flex flex-col gap-4 items-center">
             <button className="w-12 h-12 border border-white/25 bg-neutral-800 rounded text-xl" onClick={() => setFormState(!formState)}>+</button>
             {formState &&
             <form className='flex gap-5' action={handleCreate}>

bbc71c273eb138e1131fdb1cffe044d9ca199d2a
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-02 04:43:05 +0300

    update

--- src/components/AddHabitForm.tsx
+++ src/components/AddHabitForm.tsx
@@ -10,12 +10,12 @@ export default function AddHabitForm() {
 
     return (
         <div className="flex flex-col gap-4 items-center">
-            <button className="w-12 h-12 border border-white/25 bg-neutral-800 hover:bg-neutral-900 transition-colors rounded text-xl" onClick={() => setFormState(!formState)}>+</button>
+            <button className="w-12 h-12 border border-white/25 bg-neutral-800 hover:bg-neutral-900 transition-colors rounded text-xl shadow-lg" onClick={() => setFormState(!formState)}>+</button>
             {formState &&
             <form className='flex gap-5 relative' action={formAction}>
-                <input className='border border-white/50 bg-neutral-800 p-4 rounded relative' name='name' placeholder='Habit name' required maxLength={50}></input>
+                <input className='border border-white/50 bg-neutral-800 p-4 rounded relative shadow-lg' name='name' placeholder='Habit name' required maxLength={50}></input>
                 {state.error && <p className="text-red-500 text-sm absolute top-full">{state.error}</p>}
-                <button className="bg-green-700 hover:bg-green-800 transition-colors py-2 px-4 rounded" type='submit'>Add habit</button>
+                <button className="bg-green-700 hover:bg-green-800 transition-colors py-2 px-4 rounded shadow-lg" type='submit'>Add habit</button>
             </form>}
         </div>
     )

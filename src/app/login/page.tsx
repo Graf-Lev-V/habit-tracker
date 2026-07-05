f00b7e2055326cf86a7d3d9c322a57aa298e33af
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-06 02:52:30 +0300

    wip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import { signIn } from '../../lib/auth';
+import { FaGithub } from 'react-icons/fa'
 
 async function SignIn() {
   'use server';
@@ -9,9 +10,14 @@ async function SignIn() {
 export default function Login() {
   return (
     <main className='flex h-screen justify-center items-center'>
-      <div className='bg-neutral-800 p-4 rounded shadow-lg'>
+      <div className='bg-neutral-800 p-12 rounded shadow-lg'>
+        <p className='text-3xl'>Habit Traker</p>
+        <p className='text-white/75'>Track your habits, build your streaks.</p>
         <form action={SignIn}>
-          <button>Sign In</button>
+          <button className='py-2 px-4 rounded bg-green-700 hover:bg-green-800 transition-colors cursor-pointer flex gap-2 items-center'>
+            Sign in with Github
+            <FaGithub className='h-6 w-6'/>
+          </button>
         </form>
       </div>
     </main>

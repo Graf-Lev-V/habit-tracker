2947b8e86431af704ad1a813eda4ca8a8e712cea
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-07 23:47:06 +0300

    wip

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -3,16 +3,16 @@ export default function Loading() {
         <main className='p-4 flex flex-col items-center gap-4'>
             <div className='grid grid-cols-3 gap-5'>
                 <div className='px-8 py-6 rounded flex flex-col gap-3 text-center bg-neutral-700 shadow-lg animate-pulse items-center'>
-                    <div className="h-[20] w-[121] bg-neutral-600 rounded animate-pulse"></div>
-                    <div className="h-[40] w-12 bg-neutral-600 rounded animate-pulse"></div>
+                    <div className="h-[20px] w-[121px] bg-neutral-600 rounded animate-pulse"></div>
+                    <div className="h-[40px] w-12 bg-neutral-600 rounded animate-pulse"></div>
                 </div>
                 <div className='px-8 py-6 rounded flex flex-col gap-3 text-center bg-neutral-700 shadow-lg animate-pulse items-center'>
-                    <div className="h-[20] w-[121] bg-neutral-600 rounded animate-pulse"></div>
-                    <div className="h-[40] w-12 bg-neutral-600 rounded animate-pulse"></div>
+                    <div className="h-[20px] w-[121px] bg-neutral-600 rounded animate-pulse"></div>
+                    <div className="h-[40px] w-12 bg-neutral-600 rounded animate-pulse"></div>
                 </div>
                 <div className='px-8 py-6 rounded flex flex-col gap-3 text-center bg-neutral-700 shadow-lg animate-pulse items-center'>
-                    <div className="h-[20] w-[121] bg-neutral-600 rounded animate-pulse"></div>
-                    <div className="h-[40] w-12 bg-neutral-600 rounded animate-pulse"></div>
+                    <div className="h-[20px] w-[121px] bg-neutral-600 rounded animate-pulse"></div>
+                    <div className="h-[40px] w-12 bg-neutral-600 rounded animate-pulse"></div>
                 </div>
             </div>
             <div className='rounded p-4 bg-neutral-800 flex flex-col shadow-lg animate-pulse gap-2'>
@@ -23,7 +23,7 @@ export default function Loading() {
                     <div className="h-10 w-18 bg-neutral-700 rounded animate-pulse"></div>
                     <div className="h-10 w-18 bg-neutral-700 rounded animate-pulse"></div>
                 </div>
-                <div className="h-[108] w-[844] bg-neutral-700 rounded animate-pulse"></div>
+                <div className="h-[108px] w-[844px] bg-neutral-700 rounded animate-pulse"></div>
             </div>
             <div className='rounded p-4 bg-neutral-800 flex flex-col shadow-lg animate-pulse gap-2'>
                 <div className="h-5 w-32 bg-neutral-700 rounded animate-pulse"></div>
@@ -33,7 +33,7 @@ export default function Loading() {
                     <div className="h-10 w-18 bg-neutral-700 rounded animate-pulse"></div>
                     <div className="h-10 w-18 bg-neutral-700 rounded animate-pulse"></div>
                 </div>
-                <div className="h-[108] w-[844] bg-neutral-700 rounded animate-pulse"></div>
+                <div className="h-[108px] w-[844px] bg-neutral-700 rounded animate-pulse"></div>
             </div>
         </main>
     )

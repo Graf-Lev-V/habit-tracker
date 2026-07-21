070b1b7adb2ba8c92d13716e5cfc681d3f2042dc
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-21 16:00:05 +0300

    wip

--- src/components/Caledar.tsx
+++ src/components/Caledar.tsx
@@ -1,19 +1,26 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Calendar({ calendar }: { calendar: string[] }) {
 
-    const dates = []
-    const [date, setDate] = useState(() => new Date())
-
     const [days, setDays] = useState(() => typeof window !== 'undefined' ? window.innerWidth > 639 ? 365 : 90 : 90)
 
-    for (let d = 0; d <= days; d++) {
-        dates.push(date.toISOString().split('T')[0])
-        date.setDate(date.getDate() - 1);
-    }
-    const result = dates.map((date) => calendar.includes(date) ? {date: date, completed: true} : {date: date, completed: false}).reverse()
+    const [result, setResult] = useState<{date: string, completed: boolean}[]>([])
+
+    useEffect(() => {
+        const dates = []
+        const date = new Date()
+
+        for (let d = 0; d <= days; d++) {
+            dates.push(date.toISOString().split('T')[0])
+            date.setDate(date.getDate() - 1);
+        }
+
+        const newResult = dates.map((date) => calendar.includes(date) ? {date: date, completed: true} : {date: date, completed: false}).reverse()
+
+        if (JSON.stringify(result) !== JSON.stringify(newResult)) setResult(newResult)
+    }, [calendar, days, result])
 
     return (
         <div className='calendar border-t border-white/10 pt-3 mt-1'>

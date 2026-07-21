978adda6386e6022593ceadce6edf9155d1c4893
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-21 16:24:00 +0300

    wip

--- src/components/Caledar.tsx
+++ src/components/Caledar.tsx
@@ -2,24 +2,30 @@
 
 import { useEffect, useState } from "react"
 
+function generateCalendarResult(calendar: string[], days: number) {
+    const dates = []
+    const date = new Date()
+
+    for (let d = 0; d <= days; d++) {
+        dates.push(date.toISOString().split('T')[0])
+        date.setDate(date.getDate() - 1);
+    }
+
+    return dates.map((date) => calendar.includes(date) ? {date: date, completed: true} : {date: date, completed: false}).reverse()
+}
+
 export default function Calendar({ calendar }: { calendar: string[] }) {
 
     const [days, setDays] = useState(() => typeof window !== 'undefined' ? window.innerWidth > 639 ? 365 : 90 : 90)
 
-    const [result, setResult] = useState<{date: string, completed: boolean}[]>([])
+    const [result, setResult] = useState<{date: string, completed: boolean}[]>(() => generateCalendarResult(calendar, days))
 
     useEffect(() => {
-        const dates = []
-        const date = new Date()
 
-        for (let d = 0; d <= days; d++) {
-            dates.push(date.toISOString().split('T')[0])
-            date.setDate(date.getDate() - 1);
-        }
-
-        const newResult = dates.map((date) => calendar.includes(date) ? {date: date, completed: true} : {date: date, completed: false}).reverse()
-
-        if (JSON.stringify(result) !== JSON.stringify(newResult)) setResult(newResult)
+        const newResult = generateCalendarResult(calendar, days)
+        
+        if (JSON.stringify(newResult) !== JSON.stringify(result)) setResult(newResult)
+        
     }, [calendar, days, result])
 
     return (

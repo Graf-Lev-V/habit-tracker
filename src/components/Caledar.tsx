06b3ddf236fa54743e9283ce26b6326197dd3f65
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-23 23:21:56 +0300

    wip

--- src/components/Caledar.tsx
+++ src/components/Caledar.tsx
@@ -7,9 +7,7 @@ export default function Calendar({ calendar }: { calendar: string[] }) {
     const oldestDate = new Date()
     oldestDate.setDate(oldestDate.getDate() - daysBack)
 
-    const extraDays = oldestDate.getDay()
-
-    const totalDays = daysBack + extraDays
+    const totalDays = daysBack
 
     for (let d = 0; d <= totalDays; d++) {
         dates.push(date.toISOString().split('T')[0])
@@ -18,19 +16,30 @@ export default function Calendar({ calendar }: { calendar: string[] }) {
 
     const result = dates.map((date) => calendar.includes(date) ? {date: date, completed: true} : {date: date, completed: false}).reverse()
 
+    const daysBefore = new Date(result[0].date).getDay()
+    for (let i = 0; i < daysBefore; i++) {
+        result.unshift({ date: '', completed: false })
+    }
+
     const totalWeeks = Math.ceil(result.length / 7)
 
     const monthLabel: { month: number, column: number }[] = []
+    
     let lastMonth = -1
 
-    result.forEach((day, index) => {
+    for (let column = 0; column < totalWeeks; column++) {
+        const week = result.slice(column * 7, column * 7 + 7)
+        const day = week.find(d => d.date)
+
+        if (!day?.date) continue
+
         const month = new Date(day.date).getMonth()
-        const column = Math.floor(index / 7)
-        if (month !== lastMonth && index % 7 === 0) {
+
+        if (month !== lastMonth) {
             monthLabel.push({ month, column })
             lastMonth = month
         }
-    })
+    }
 
     const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
@@ -42,11 +51,18 @@ export default function Calendar({ calendar }: { calendar: string[] }) {
                 )}
             </div>
             <div className='calendar border-t border-white/10 pt-3 mt-1 grid grid-rows-7 gap-1 justify-center grid-flow-col' style={{gridTemplateColumns: `repeat(${totalWeeks}, 1fr)`}}>
-                {result.map((date) =>
-                    date.completed ?
-                    <div key={date.date} className='w-4 h-4 bg-green-600 rounded-xs'></div> :
-                    <div key={date.date} className='w-4 h-4 bg-gray-700 rounded-xs'></div>
-                )}
+                {result.map((day, index) => (
+                    <div
+                        key={index}
+                        className={`w-4 h-4 rounded-xs ${
+                            !day.date
+                                ? 'bg-transparent'
+                                : day.completed
+                                    ? 'bg-green-600'
+                                    : 'bg-gray-700'
+                        }`}
+                    />
+                ))}
             </div>
         </div>
     )

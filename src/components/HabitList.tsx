e642281158a9f477853f4354dbb513be62936e41
Graf-Lev-V <legendarlev2004@gmail.com>
2026-07-30 21:24:57 +0300

    wip

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -13,22 +13,29 @@ export default function HabitList({ habitStreak }: { habitStreak: Habit[] }) {
                 initial={{opacity: 0}}
                 animate={{opacity: 1}}
                 exit={{opacity: 0, scale: 0.95}}
-                transition={{duration: 0.4}}
+                transition={{duration: 1}}
             >
                 <p className='rounded p-4 bg-neutral-800 text-white/80'>No habits yet. Add your first one to start tracking your progress.</p>
             </motion.div>
             :
-            habitStreak?.map((habit) => (
-                <motion.div 
-                    key={habit.habit.id}
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    exit={{opacity: 0, scale: 0.95}}
-                    transition={{duration: 0.4}}
-                >
-                    <HabitCard habit={habit} />
-                </motion.div>
-            ))
+            <motion.div
+                key='list'
+                className="flex flex-col gap-4"
+            >
+                <AnimatePresence>
+                {habitStreak?.map((habit) => (
+                    <motion.div 
+                        key={habit.habit.id}
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        exit={{opacity: 0, scale: 0.95}}
+                        transition={{duration: 1}}
+                    >
+                        <HabitCard habit={habit} />
+                    </motion.div>
+                ))}
+                </AnimatePresence>
+            </motion.div>
             }
         </AnimatePresence>
     )

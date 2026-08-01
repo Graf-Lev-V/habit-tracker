962f9ad0a54f4f838d944fb4fbafc5e74766436e
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-01 12:26:52 +0300

    fix

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -5,15 +5,15 @@ import Calendar from './Caledar'
 
 export default function HabitCard({ habit }: { habit: Habit }) {
     return (
-        <div className='rounded p-4 bg-neutral-800 hover:brightness-110 flex flex-col shadow-lg w-full sm:w-max min-w-0'>
-            <div className='flex justify-between'>
-                <p className='text-white text-xl truncate' title={habit.habit.name}>{habit.habit.name}</p>
+        <div className='rounded p-4 bg-neutral-800 hover:brightness-110 flex flex-col shadow-lg w-full sm:w-max min-w-0 max-w-full'>
+            <div className='flex justify-between min-w-0'>
+                <p className='text-white text-xl truncate min-w-0' title={habit.habit.name}>{habit.habit.name}</p>
                 <HabitDelete id={habit.habit.id} />
             </div>
-            <p className='text-white/60 text-sm'>Streak: <span className='text-green-500 font-semibold'>{habit.streak}</span></p>
-            <p className='text-white/60 text-sm'>Thirty-day complection: {habit.thirtyDay}%</p>
+            <p className='text-white/60 text-sm min-w-0'>Streak: <span className='text-green-500 font-semibold'>{habit.streak}</span></p>
+            <p className='text-white/60 text-sm min-w-0'>Thirty-day complection: {habit.thirtyDay}%</p>
             <HabitToggle id={habit.habit.id} completedToday={habit.completedToday} />
-            {/* <Calendar calendar={habit.calendar}/> */}
+            <Calendar calendar={habit.calendar}/>
         </div>
     )
 }

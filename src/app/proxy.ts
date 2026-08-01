10ece5fe864a9b962b28985981cfb14cf218e3c9
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-01 23:39:10 +0300

    fix

--- src/app/proxy.ts
+++ src/app/proxy.ts
@@ -1,13 +1,10 @@
-import { auth } from '../lib/auth'
 import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
-export default auth((req) => {
-    if (!req.auth) {
-        const loginUrl = new URL('/login', req.url)
-        return NextResponse.redirect(loginUrl)
-    }
-    return NextResponse.next()
-})
+export function proxy(req: NextRequest) {
+    console.log('PROXY DEFINITELY RAN')
+    return NextResponse.redirect(new URL('/login', req.url))
+}
 
 export const config = {
     matcher: ['/dashboard']

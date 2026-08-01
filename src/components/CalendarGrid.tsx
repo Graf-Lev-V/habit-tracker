ab87d76b02b2a1b449da1d304df032622c5493eb
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-01 11:24:34 +0300

    fix

--- src/components/CalendarGrid.tsx
+++ src/components/CalendarGrid.tsx
@@ -3,7 +3,7 @@ export default function CalendarGrid({ result, totalWeeks, visibleLabels, monthN
         <>
             <div className="grid grid-rows-1 gap-1 justify-center " style={{gridTemplateColumns: `repeat(${totalWeeks}, clamp(10px, 4vw, 16px))`}}>
                 {visibleLabels.map((ml) => 
-                    <div key={ml.column} className="w-4 text-white/70 text-sm min-w-0 overflow-hidden whitespace-nowrap" style={{gridColumnStart: ml.column + 1}}>{monthNames[ml.month]}</div>
+                    <div key={ml.column} className="w-full text-white/70 text-sm min-w-0 overflow-hidden whitespace-nowrap" style={{gridColumnStart: ml.column + 1}}>{monthNames[ml.month]}</div>
                 )}
             </div>
             <div className='grid grid-rows-7 gap-1 justify-center grid-flow-col' style={{gridTemplateColumns: `repeat(${totalWeeks}, clamp(10px, 4vw, 16px))`}}>

f30f0f242dd3e1d9aeb96ea74d5a895d2be2c4cb
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-02 23:48:31 +0300

    fix

--- src/components/HabitName.tsx
+++ src/components/HabitName.tsx
@@ -8,11 +8,17 @@ import { toast } from "sonner";
 export default function HabitName({ id, habitName }: { id: string, habitName: string }) {
 
     const [isEditing, setIsEditing] = useState<boolean>(false)
+    const [displayName, setDisplayName] = useState(habitName)
 
     const handleBlur = async (e: React.FocusEvent<HTMLInputElement>) => {
-        const result = await updateHabitName(id, !e.target.value.trim() ? habitName : e.target.value)
-        if (result.error) toast.error(result.error)
+        const newValue = !e.target.value.trim() ? habitName : e.target.value
+        setDisplayName(newValue)
         setIsEditing(false)
+        const result = await updateHabitName(id, newValue)
+        if (result.error) { 
+            toast.error(result.error)
+            setDisplayName(habitName)
+        }
     }
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') e.currentTarget.blur()
@@ -24,7 +30,7 @@ export default function HabitName({ id, habitName }: { id: string, habitName: st
             <input className="border border-white/50 bg-neutral-900 rounded text-xl px-2 w-full" defaultValue={habitName} onBlur={handleBlur} onKeyDown={handleKeyDown} maxLength={50} autoFocus/>
             :
             <div className="flex items-center gap-2 w-max cursor-pointer" onClick={() => setIsEditing(true)}>
-                <p className='text-white text-xl truncate' title={habitName}>{habitName}</p>
+                <p className='text-white text-xl truncate' title={habitName}>{displayName}</p>
                 <FaPen className="text-sm text-white/60"></FaPen>
             </div>
         }

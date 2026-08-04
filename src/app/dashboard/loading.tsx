b1485a28e2ce4787693816815749cae9b09b51dd
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-04 12:57:19 +0300

    fix

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -17,7 +17,7 @@ export default function Loading() {
             </div>
             <div className='h-[311px] sm:h-[333px] w-full max-w-6xl rounded p-4 bg-neutral-800 flex flex-col shadow-lg animate-pulse'>
                 <div className="h-8 flex justify-between items-center gap-2 mb-3">
-                    <div className="h-[28px] w-full bg-neutral-700 rounded animate-pulse"></div>
+                    <div className="h-8 w-full bg-neutral-700 rounded animate-pulse"></div>
                     <div className="aspect-square p-4 rounded bg-neutral-700"></div>
                 </div>
                 <div className="h-[12px] w-[55px] mb-2 bg-neutral-700 rounded animate-pulse"></div>

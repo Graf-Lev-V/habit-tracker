bcc337afb8d781111e529d0e50a747226d62be05
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-04 10:48:53 +0300

    fix

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -21,7 +21,18 @@ export default function Test() {
             </div>
             <div className='h-[311px] sm:h-[333px] w-full max-w-6xl rounded p-4 bg-neutral-800 flex flex-col shadow-lg animate-pulse'>
                 <div className="flex justify-between items-center gap-2">
-                    <div className="h-[32px] flex-1 mt-1 mb-3 bg-neutral-700 rounded animate-pulse"></div>
+                    <div className="h-[32px] w-full mt-1 mb-3 bg-neutral-700 rounded animate-pulse"></div>
+                    <div className="aspect-square p-2 mb-1 rounded bg-neutral-700"></div>
+                </div>
+                <div className="h-[12px] w-[55px] mb-2 bg-neutral-700 rounded animate-pulse"></div>
+                <div className="h-[12px] w-[175px] mb-1 bg-neutral-700 rounded animate-pulse"></div>
+                <div className="h-[40px] w-[70px] my-2 mb-[41px] bg-neutral-700 rounded animate-pulse"></div>
+                <div className="hidden sm:block h-[136px] w-full mt-1 bg-neutral-700 rounded animate-pulse"></div>
+                <div className="block sm:hidden h-[114px] w-full mt-1 bg-neutral-700 rounded animate-pulse"></div>
+            </div>
+            <div className='h-[311px] sm:h-[333px] w-full max-w-6xl rounded p-4 bg-neutral-800 flex flex-col shadow-lg animate-pulse'>
+                <div className="flex justify-between items-center gap-2">
+                    <div className="h-[32px] w-full mt-1 mb-3 bg-neutral-700 rounded animate-pulse"></div>
                     <div className="aspect-square p-2 mb-1 rounded bg-neutral-700"></div>
                 </div>
                 <div className="h-[12px] w-[55px] mb-2 bg-neutral-700 rounded animate-pulse"></div>

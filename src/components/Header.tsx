234a6c8a72b1af1fc8ed98a0a9aa0736ab10c888
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-04 11:34:09 +0300

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default async function Header() {
     const session = await auth()
     return (
         <header>
-            <div className='flex items-center py-2 px-4 border-b-1 border-white/50 gap-4 pr-[25px]'>
+            <div className='flex items-center py-2 px-4 border-b-1 border-white/50 gap-4'>
                 <Image className='rounded-full' priority src={session?.user?.image ?? ''} alt='user image' width={64} height={64}/>
                 <p>{session?.user?.name}</p>
                 <SignOut className='ml-auto'/>

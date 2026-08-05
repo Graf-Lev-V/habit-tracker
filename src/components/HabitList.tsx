95eaa2d46368a2913e89f4ba16d954ce70bc8c1e
Graf-Lev-V <legendarlev2004@gmail.com>
2026-08-05 18:05:41 +0300

    fix

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -16,7 +16,7 @@ export default function HabitList({ habitStreak }: { habitStreak: Habit[] }) {
 
     return (
             <div className="flex flex-col gap-4 justify-center items-center w-full max-w-[450px] sm:max-w-6xl">
-                <AnimatePresence onExitComplete={() => {
+                <AnimatePresence initial={false} onExitComplete={() => {
                     if (habitStreak.length === 0) {
                         setShowEmpty(true)
                         setShowCards(false)
@@ -34,7 +34,7 @@ export default function HabitList({ habitStreak }: { habitStreak: Habit[] }) {
                         </motion.div>
                     ))}
                 </AnimatePresence>
-                <AnimatePresence onExitComplete={() => {
+                <AnimatePresence initial={false} onExitComplete={() => {
                     if (habitStreak.length > 0) setShowCards(true)
                 }}>
                     {showEmpty &&
